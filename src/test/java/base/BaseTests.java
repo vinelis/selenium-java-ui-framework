5b11cfc522cec92022434a9ea38c473ac66cfd16
Luca Vinelli <lucavinelli4@gmail.com>
2025-09-01 13:37:58 -0300

    refactor: Parameterize BaseTests (browser, headless, baseUrl)

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,6 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
@@ -14,10 +12,24 @@ public class BaseTests {
 
     @BeforeMethod
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        String browser  = System.getProperty("browser", "chrome");
+        boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        String baseUrl  = System.getProperty("baseUrl", "https://osvaldovinelli.com.ar/");
+
+        if ("chrome".equalsIgnoreCase(browser)) {
+            io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+            org.openqa.selenium.chrome.ChromeOptions options = new org.openqa.selenium.chrome.ChromeOptions();
+            if (headless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--window-size=1920,1080");
+            }
+            driver = new org.openqa.selenium.chrome.ChromeDriver(options);
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
         driver.manage().window().maximize();
-        driver.get("https://osvaldovinelli.com.ar/");
+        driver.get(baseUrl);
         homePage = new HomePage(driver);
     }
 
